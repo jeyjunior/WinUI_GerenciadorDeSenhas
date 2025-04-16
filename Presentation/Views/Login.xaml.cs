249d857c1c09d550ad94edd63bd3ab60b8dd3b1b
José Junior <jeyjr.dg@outlook.com>
2025-04-16 17:48:16 -0300

    Implementado melhoria na segurança da chave master para criptografia

---
+++ Presentation/Views/Login.xaml.cs
@@ -0,0 +1,30 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace Presentation.Views
+{
+    public sealed partial class Login : Page
+    {
+        public Login()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnLoginGoogle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
