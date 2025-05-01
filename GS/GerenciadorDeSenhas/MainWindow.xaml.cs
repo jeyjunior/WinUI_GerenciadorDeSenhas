e42fd3100dccc84cbe232b73e829e374da75642a
jeyjunior <jeyjuniordg@gmail.com>
2025-05-01 18:52:04 -0300

    Add logo no TiTle Bar

--- GS/GerenciadorDeSenhas/MainWindow.xaml.cs
+++ GS/GerenciadorDeSenhas/MainWindow.xaml.cs
@@ -18,16 +18,32 @@ using GSApplication.Services;
 using Windows.UI.Popups;
 using Windows.Storage;
 
+using Microsoft.UI;           // Needed for WindowId.
+using Microsoft.UI.Windowing; // Needed for AppWindow.
+using WinRT.Interop;          // Needed for XAML/HWND interop.
+
 namespace GerenciadorDeSenhas
 {
     public sealed partial class MainWindow : Window
     {
+        private AppWindow m_AppWindow;
         public MainWindow()
         {
             this.InitializeComponent();
 
+            m_AppWindow = GetAppWindowForCurrentWindow();
+            m_AppWindow.Title = "Gerenciador de Senhas";
+            m_AppWindow.SetIcon("Assets/gerenciador_de_senhas.ico");
+
             NavigationService.MainFrame = this.MainFrame;
             NavigationService.NavegarPara(typeof(Login));
+        }
+
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
         }
     }
 }
